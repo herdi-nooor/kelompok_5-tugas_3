b32bf543f3c0b2768e8a547342576550594944ad
Muhamad Faisal <61342260+Icankkkk@users.noreply.github.com>
2022-08-13 16:47:28 +0800

    add boot setting

---
+++ Assets/Script/Boot/GameMain.cs
@@ -0,0 +1,26 @@
+using Agate.MVC.Base;
+using Agate.MVC.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceInvander.Boot
+{
+    public class GameMain : BaseMain<GameMain>, IMain
+    {
+        protected override IConnector[] GetConnectors()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IController[] GetDependencies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override IEnumerator StartInit()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Script/Boot/SceneLoader.cs
@@ -0,0 +1,13 @@
+using Agate.MVC.Base;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceInvander.Boot
+{
+    public class SceneLoader : BaseLoader<SceneLoader>
+    {
+        protected override string SplashScene => throw new System.NotImplementedException();
+    }
+
+}
